ff1c8d1b3990875ae8224dff09efdf60f0401be1
Rajporiya <mrraj4129@gmail.com>
2026-07-14 22:21:56 +0530

    add reusable role constants

---
+++ server/config/roles.js
@@ -0,0 +1,11 @@
+export const ROLES = {
+  COMMITTEE_HEAD: "Committee Head",
+  COMMITTEE_MEMBER: "Committee Member",
+  RESIDENT: "Resident",
+  SECURITY_GUARD: "Security Guard",
+}
+
+export const ALL_ROLES = Object.values(ROLES)
+export const DEFAULT_ROLE = ROLES.RESIDENT
+
+export default ROLES
